33a02097da0d0dd0bd70f206a181791298858432
Andrew King <pepsiotaku@gmail.com>
2019-02-07 23:19:37 -0800

    big update

--- build.js
+++ build.js
@@ -1,31 +1,32 @@
 const { writeFile, readFileSync } = require('fs');
 const yaml = require('js-yaml');
 
-function themeBuilder(folder, outputFile) 
+function themeBuilder(folder, outputFile)
 {
     // Panda theme color definition
     let themeColors = yaml.safeLoad(readFileSync(folder + '/colors.yaml', 'utf-8'));
-    
+
     // Base has the syntax tokens applicable across multiple languages
     let base = yaml.safeLoad(readFileSync(folder + '/base.yaml', 'utf-8'));
-    
+
     // Additional theme definitions to combine with base syntax token styles
     let workbench = yaml.safeLoad(readFileSync(folder + '/workbench.yaml', 'utf-8'));
-    
+
     // Merge workbench styles
     Object.assign(base, workbench);
 
     // Merge additional syntax token styles
     base.tokenColors = base.tokenColors.concat(
-        yaml.safeLoad(readFileSync(folder + '/template.yaml', 'utf-8')),
+        yaml.safeLoad(readFileSync(folder + '/javascript.yaml', 'utf-8')),
+        yaml.safeLoad(readFileSync(folder + '/typescript.yaml', 'utf-8')),
         yaml.safeLoad(readFileSync(folder + '/markdown.yaml', 'utf-8')),
         yaml.safeLoad(readFileSync(folder + '/hexdump.yaml', 'utf-8')),
-        yaml.safeLoad(readFileSync(folder + '/html.yaml', 'utf-8')),
         yaml.safeLoad(readFileSync(folder + '/css.yaml', 'utf-8')),
         yaml.safeLoad(readFileSync(folder + '/cpp.yaml', 'utf-8')),
-        yaml.safeLoad(readFileSync(folder + '/javascript.yaml', 'utf-8')),
+        yaml.safeLoad(readFileSync(folder + '/makefile.yaml', 'utf-8')),
+        yaml.safeLoad(readFileSync(folder + '/shell.yaml', 'utf-8')),
         yaml.safeLoad(readFileSync(folder + '/graphql.yaml', 'utf-8')),
-        yaml.safeLoad(readFileSync(folder + '/python.yaml', 'utf-8')),
+        //yaml.safeLoad(readFileSync(folder + '/python.yaml', 'utf-8')),
         yaml.safeLoad(readFileSync(folder + '/regex.yaml', 'utf-8')),
         yaml.safeLoad(readFileSync(folder + '/json.yaml', 'utf-8')),
         yaml.safeLoad(readFileSync(folder + '/jsdoc.yaml', 'utf-8')),
