13a5183018685fdebfef76db9be2a32e8c70197b
Andrew King <pepsiotaku@gmail.com>
2019-08-10 18:34:26 -0400

    ts > js, fixed theme json warnings

---
+++ src/build.ts
@@ -0,0 +1,49 @@
+import { writeFile, readFileSync } from "fs";
+import yaml from "js-yaml";
+
+const buildTheme = (inFolder: string, inOutputFile: string): void => {
+    const yamlFile: any = (file: string): any => {
+        return yaml.safeLoad(readFileSync(`./src/${inFolder}/${file}.yaml`, "utf-8"));
+    };
+
+    const themeColors = yamlFile("colors");
+    const workbench = yamlFile("workbench");
+
+    let base = yamlFile("base");
+    Object.assign(base, workbench);
+
+    base.tokenColors = base.tokenColors.concat(
+        yamlFile("cmake"),
+        yamlFile("cpp"),
+        yamlFile("css"),
+        yamlFile("glsl"),
+        yamlFile("graphql"),
+        yamlFile("hexdump"),
+        yamlFile("javascript"),
+        yamlFile("jsdoc"),
+        yamlFile("json"),
+        yamlFile("lua"),
+        yamlFile("makefile"),
+        yamlFile("markdown"),
+        yamlFile("python"),
+        yamlFile("regex"),
+        yamlFile("shell"),
+        yamlFile("typescript"),
+        yamlFile("yaml")
+    );
+
+    base = JSON.stringify(base, null, 0);
+
+    for (let color in themeColors) {
+        const re = new RegExp(color + '"', "g");
+        base = base.replace(re, themeColors[color] + '"');
+    }
+
+    writeFile("./dist/" + inOutputFile + ".json", base, (err) => {
+        if (err) console.warn(err);
+
+        console.log("Build finished: " + inOutputFile);
+    });
+};
+
+buildTheme("default", "RetroFuture");
